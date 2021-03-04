b43aa79e482bcec8f35981a27ef448b6f9361e00
aria22408 <tein408@gmail.com>
2021-03-04 20:00:38 +0900

    feat:hard coding된 token 수정

--- src/main/java/teinBot/MainApp.java
+++ src/main/java/teinBot/MainApp.java
@@ -2,17 +2,17 @@ package teinBot;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-
 import javax.security.auth.login.LoginException;
 
-public class MainAppEx {
+public class MainApp {
 
     public static void main(String[] args) throws LoginException{
         JDA jda = JDABuilder
-                .createDefault("tokenHere")
+                .createDefault(Token.getToken())
                 .build();
         jda.addEventListener(new MessageListener());
 
-
     }//main
+
+
 }//class
