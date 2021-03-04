e80f1d43bc17a356984fe3e29d5315913eca42af
aria22408 <tein408@gmail.com>
2021-03-04 14:49:26 +0900

    feat:$help추가, $delete시 사용되는 코드 try-catch안으로 이동

--- src/main/java/teinBot/MessageListener.java
+++ src/main/java/teinBot/MessageListener.java
@@ -1,38 +1,66 @@
 package teinBot;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
 
+import java.awt.*;
+import java.time.OffsetDateTime;
 import java.util.List;
 
 public class MessageListener extends ListenerAdapter {
 
-    @Override
+   @Override
     public void onMessageReceived(MessageReceivedEvent event) {
 
         User author = event.getAuthor();
         Message message = event.getMessage();
         MessageChannel channel = event.getChannel();
         TextChannel tc = event.getTextChannel();
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+
         String msg = message.getContentDisplay();
+        //System.out.println("author : "+author);
+        //System.out.println("channel : "+channel);
+        //System.out.println("message : "+msg);
+        //System.out.println("message.getId() : "+message.getId());
 
         if(author.isBot()) return;
-        if(msg.equals("!ping")) {
-            channel.sendMessage("Pong!").queue();
+        if(msg.equalsIgnoreCase("$help")) {
+            embedBuilder.setColor(new Color(66, 98, 140));
+            //embedBuilder.setAuthor("setAuthor");
+            embedBuilder.setTitle("teinBot");
+            embedBuilder.setDescription("How to use teinBot?");
+            //embedBuilder.addBlankField(true);
+            embedBuilder.addField("$help", "도움말 출력",false);
+            embedBuilder.addField("$ping", "Pong! 출력",false);
+            embedBuilder.addField("$echo [int]", "int 만큼 [hello : int] 출력",false);
+            embedBuilder.addField("$delete [int]", "int 만큼 메시지 삭제",false);
+            embedBuilder.setFooter("made by 이안#8990");
+            embedBuilder.setTimestamp(OffsetDateTime.now());
+            channel.sendMessage(embedBuilder.build()).queue();
         }
         else {
-            if(msg.length() != 0 && msg.charAt(0) == '!'){
+            if(msg.length() != 0 && msg.charAt(0) == '$'){
                 String[] args = message.getContentRaw().substring(1).split(" ");
                 int count;
 
                 if(args.length <= 0) return;
+                if(args[0].equalsIgnoreCase("ping")){
+                    channel.sendMessage("Pong!").queue();
+                }
+
+
                 if(args[0].equalsIgnoreCase("echo")){
 
                     try{
                         count = Integer.parseInt(args[1]);
                     } catch (Exception e) {
-                        channel.sendMessage("int input pls").queue();
+                        channel.sendMessage("input int pls").queue();
                         return;
                     }
                     for(int i=0; i<count; i++){
@@ -60,15 +88,21 @@ public class MessageListener extends ListenerAdapter {
                     int share = count/100;
                     int rest = count%100;
 
-                    for(int i = 0; i<share; i++){
-                        messages = messageHistory.retrievePast(100).complete();
+                    try{
+                        for(int i = 0; i<share; i++){
+                            messages = messageHistory.retrievePast(100).complete();
+                            tc.deleteMessages(messages).complete();
+                        }
+
+                        messages = messageHistory.retrievePast(rest+1).complete();
                         tc.deleteMessages(messages).complete();
+
+                        channel.sendMessage(count + " was deleted").queue();
+
+                    } catch (IllegalArgumentException e) {
+                        e.printStackTrace();
                     }
 
-                    messages = messageHistory.retrievePast(rest+1).complete();
-                    tc.deleteMessages(messages).complete();
-
-                    channel.sendMessage(count + " was deleted").queue();
                 }
             }
         }
