5530ab61ac52f2f6aa0910cc8746e52a360d8f28
aria22408 <tein408@gmail.com>
2021-03-07 15:05:24 +0900

    chore: 클래스 이름 변경

--- src/main/java/teinBot/DeleteMessage.java
+++ src/main/java/teinBot/DeleteMessage.java
@@ -6,7 +6,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.util.List;
 
-public class DeleteMessages extends ListenerAdapter {
+public class DeleteMessage extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
