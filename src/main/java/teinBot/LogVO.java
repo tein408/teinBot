4472aebad0d0d536db063954802dfc892ebda122
aria22408 <tein408@gmail.com>
2021-03-08 23:11:01 +0900

    feat: LogVO 생성

---
+++ src/main/java/teinBot/LogVO.java
@@ -0,0 +1,91 @@
+package teinBot;
+
+public class LogVO {
+
+    private String author;
+    private String name;
+    private String channel;
+    private String chaneelid;
+    private String message;
+    private String changed;
+    private String delete;
+
+    public LogVO(String author, String name, String channel, String chaneelid, String message, String changed, String delete) {
+        this.author = author;
+        this.name = name;
+        this.channel = channel;
+        this.chaneelid = chaneelid;
+        this.message = message;
+        this.changed = changed;
+        this.delete = delete;
+    }
+
+    @Override
+    public String toString() {
+        return "LogVO{" +
+                "author='" + author + '\'' +
+                ", name='" + name + '\'' +
+                ", channel='" + channel + '\'' +
+                ", chaneelid='" + chaneelid + '\'' +
+                ", message='" + message + '\'' +
+                ", changed='" + changed + '\'' +
+                ", delete='" + delete + '\'' +
+                '}';
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+
+    public String getChaneelid() {
+        return chaneelid;
+    }
+
+    public void setChaneelid(String chaneelid) {
+        this.chaneelid = chaneelid;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getChanged() {
+        return changed;
+    }
+
+    public void setChanged(String changed) {
+        this.changed = changed;
+    }
+
+    public String getDelete() {
+        return delete;
+    }
+
+    public void setDelete(String delete) {
+        this.delete = delete;
+    }
+}//class
