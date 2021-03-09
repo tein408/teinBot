d7da059fd3b5d5994f377a11ee16454ecb5fef71
aria22408 <tein408@gmail.com>
2021-03-09 20:31:30 +0900

    feat:수정된 chat log를 특정 채널에 업로드, DB에 저장

---
+++ src/main/java/teinBot/EditLog.java
@@ -0,0 +1,45 @@
+package teinBot;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.time.OffsetDateTime;
+
+public class EditLog extends ListenerAdapter {
+
+    LogVO logVO = new LogVO();
+    LogDAO logDAO = new LogDAO();
+
+    @Override
+    public void onMessageUpdate(@NotNull MessageUpdateEvent event) {
+        User user = event.getMember().getUser();
+
+        if(event.getAuthor().isBot()){
+            return;
+        }
+
+        EmbedBuilder update = new EmbedBuilder();
+        update.setColor(Color.GRAY);
+        update.setAuthor(event.getAuthor().getAsTag(), null, user.getAvatarUrl());
+        update.setTitle("Message edited at #"+event.getChannel().getName());
+
+        update.addField("Test", logDAO.selectBeforeMessage(event.getMessageId()),false);
+        update.addField("After", event.getMessage().getContentRaw(),false);
+        update.setTimestamp(OffsetDateTime.now());
+
+        logVO.setChanged(event.getMessage().getContentRaw());
+        logVO.setMessageId(event.getMessageId());
+        System.out.println("test : " + logVO.getChanged() + " "+ logVO.getMessageId());
+        logDAO.updateDB(logVO);
+
+        event.getGuild().getTextChannelsByName("update-message-log",false)
+                .get(0).sendMessage(update.build()).queue();
+
+    }//onMessageUpdate
+
+}//class
